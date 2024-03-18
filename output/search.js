8a9e98f1b166b467c5be126d2cc575a925195aae
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-03-18 18:09:29 +0530

    Update search.js

--- output/search.js
+++ output/search.js
@@ -3,17 +3,19 @@ $(document).ready(function() {
     function filterResults(searchQuery) {
         var filteredResults = [];
         var pages = [
-            { title: 'lorem', url: '/lorem.html' },
-            { title: 'about', url: '/about.html' },
+            { title: 'Page 1', url: '/page1.html' },
+            { title: 'Page 2', url: '/page2.html' },
             { title: 'Page 3', url: '/page3.html' }
         ];
 
         // Filter pages based on search query
-        pages.forEach(function(page) {
-            if (page.title.toLowerCase().includes(searchQuery.toLowerCase())) {
-                filteredResults.push(page);
-            }
-        });
+        if (searchQuery.trim() !== '') { // Only filter if search query is not empty
+            pages.forEach(function(page) {
+                if (page.title.toLowerCase().includes(searchQuery.toLowerCase())) {
+                    filteredResults.push(page);
+                }
+            });
+        }
 
         return filteredResults;
     }
@@ -31,6 +33,15 @@ $(document).ready(function() {
                 resultsContainer.append(link).append('<br>');
             });
         }
+    }
+
+    // Event listener for search input keyup event
+    $('#searchInput').on('keyup', function() {
+        var searchQuery = $(this).val();
+        var filteredResults = filterResults(searchQuery);
+        displayResults(filteredResults);
+    });
+});
     }
 
     // Event listener for search input keyup event
